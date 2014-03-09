cd1a10d93550ec4d62f9923884e4e1c0cb1408ab
Dalan Mendonca <dalan.dudefunk@gmail.com>
2014-03-09 19:02:13 +0530

    Adding Timeline maker - alpha
    All templates and code for timeline making

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from flask import Flask, render_template, request, redirect
 import hashlib
 import random
+import json
 from google.appengine.ext import db
 
 class ShortenedURLs(db.Model):
   hashedURL = db.StringProperty()
   originalURL = db.StringProperty()
   shortURL = db.StringProperty()
+
+class timelineData(db.Model):
+  username = db.StringProperty()
+  md5password = db.StringProperty()
+  timelineConfig = db.TextProperty()  
 
 app = Flask(__name__)
 alphanumeric = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','1','2','3','4','5','6','7','8','9','0']
@@ -95,6 +101,97 @@ def handle_short_input(shortinput):
     	else:
     		destination = "http://" + destination
     	return redirect(destination)
+
+@app.route('/t')
+def t():
+    return render_template('t.htm')
+
+@app.route('/tnew', methods = ['POST'])
+def tnew():
+	uname = request.form["username"]
+	pwd = request.form["password"]
+	tdb = timelineData.all()
+	tdb.filter("username =",uname)
+	results = tdb.fetch(1)
+	if results==[]:
+		print "Username hasn't been used, awesome!"
+		hashObject = hashlib.md5(pwd)
+		urlHash = hashObject.hexdigest()	
+		hashpwd = hashlib.md5(pwd).hexdigest()
+		print uname
+		print pwd
+		print hashpwd
+
+		new_timeline = timelineData(username=uname,md5password=hashpwd,timelineConfig="#UnconfiguredTimeline")
+		new_timeline.put()
+		return render_template('teditor.htm',new_user=True, uname=uname)
+	else:
+		print "Username is already taken"
+		return render_template('terror.htm',errorcode=1)
+
+@app.route('/treturning', methods = ['POST'])
+def treturning():
+	uname = request.form["username"]
+	tdb = timelineData.all()
+	tdb.filter("username =",uname)
+	results = tdb.fetch(1)
+	if results==[]:
+		print "Username doesn't exist"
+		return render_template('terror.htm',errorcode=2)
+	else:
+		x =  results[0]
+		#print x.username
+		#print x.md5password
+		#print x.timelineConfig
+		if x.timelineConfig=="#UnconfiguredTimeline":
+			configured=False
+			return render_template('teditor.htm',new_user=True)
+		else:
+			return render_template('teditor.htm',new_user=False, configdata=x.timelineConfig, uname=uname)
+
+@app.route('/tconfigprocessor', methods = ['POST'])
+def tconfigprocessor():		
+	inputconfig = request.form["userconfig"]
+	uname = request.form["username"]
+	pwd = request.form["password"]
+	hashpwd = hashlib.md5(pwd).hexdigest()
+	tdb = timelineData.all()
+	tdb.filter("username =",uname)
+	results = tdb.fetch(1)
+	x =  results[0]
+	dbpwd = x.md5password
+	if hashpwd==dbpwd:
+		print "Passwords match!"
+		#print type(x)
+		x.timelineConfig = inputconfig
+		x.put()
+		destination = x.username
+		destination = "/t/" + uname
+		return redirect(destination)
+	else:
+		print "whoops your password was wrong :(, try again"
+		return render_template('teditor.htm',new_user=False, configdata=inputconfig, uname=uname)
+	#print inputconfig
+
+@app.route('/t/<username>')
+def handle_timeline(username):
+	# The pattern below is used repeatedly, make a function out of it when efficiency matters
+	tdb = timelineData.all()
+	tdb.filter("username =",username)
+	results = tdb.fetch(1)
+	#results = tdb.fetch(1)
+	if results==[]:
+		print "Username doesn't exist"
+		return render_template('terror.htm',errorcode=2)
+	else:
+		x =  results[0]
+		configdata = x.timelineConfig
+		print "Valid Username, attempting render"
+		#configdata = configdata.replace('\n','***')
+		configdata = json.dumps(configdata)
+		return render_template('ttimeline.htm',configdata=configdata)
+
+
 
 
 
